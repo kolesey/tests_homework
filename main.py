df8ff8320e3c97d8bd2f9f71ea8bd22afee5ae0f
Garik <igor@koleso40.ru>
2026-01-21 16:54:06 +0300

    первое задание выполнено

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ def solve(models: list, available: list, manufacturers: list):
 
     return ssds, repair_count  # Этот код менять не нужно
 
-models = ['480 ГБ 2.5" SATA накопитель Kingston A400', '500 ГБ 2.5" SATA накопитель Samsung 870 EVO',
-              '480 ГБ 2.5" SATA накопитель ADATA SU650', '240 ГБ 2.5" SATA накопитель ADATA SU650',
-              '250 ГБ 2.5" SATA накопитель Samsung 870 EVO', '256 ГБ 2.5" SATA накопитель Apacer AS350 PANTHER',
-              '480 ГБ 2.5" SATA накопитель WD Green', '500 ГБ 2.5" SATA накопитель WD Red SA500']
-available = [1, 1, 1, 1, 0, 1, 1, 0]
-manufacturers = ['Intel', 'Samsung', 'WD']
 
-print(solve(models, available, manufacturers))
+def reverse(string: str) -> str:
+    return string[::-1].lower()
+
+
+def check_email(email: str) -> bool:
+    if ' ' not in email and '@' in email and '.' in email:
+        return True
+    else:
+        return False
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,31 +1,76 @@
 import pytest
 
-from main import solve
+from main import solve, reverse, check_email
 
 
+# Test solve
 @pytest.fixture
 def create_list_of_models():
     models = ['480 ГБ 2.5" SATA накопитель Kingston A400',
-                '500 ГБ 2.5" SATA накопитель Samsung 870 EVO',
-                '480 ГБ 2.5" SATA накопитель ADATA SU650',
-                '240 ГБ 2.5" SATA накопитель ADATA SU650',
-                '250 ГБ 2.5" SATA накопитель Samsung 870 EVO',
-                '256 ГБ 2.5" SATA накопитель Apacer AS350 PANTHER',
-                '480 ГБ 2.5" SATA накопитель WD Green',
-                '500 ГБ 2.5" SATA накопитель WD Red SA500']
+              '500 ГБ 2.5" SATA накопитель Samsung 870 EVO',
+              '480 ГБ 2.5" SATA накопитель ADATA SU650',
+              '240 ГБ 2.5" SATA накопитель ADATA SU650',
+              '250 ГБ 2.5" SATA накопитель Samsung 870 EVO',
+              '256 ГБ 2.5" SATA накопитель Apacer AS350 PANTHER',
+              '480 ГБ 2.5" SATA накопитель WD Green',
+              '500 ГБ 2.5" SATA накопитель WD Red SA500']
     return models
 
 
 params = (
-    ([1, 1, 1, 1, 0, 1, 1, 0], ['Intel', 'Samsung', 'WD'], (['500 ГБ 2.5" SATA накопитель Samsung 870 EVO', '480 ГБ 2.5" SATA накопитель WD Green'], 2)),
+    ([1, 1, 1, 1, 0, 1, 1, 0], ['Intel', 'Samsung', 'WD'],
+     (['500 ГБ 2.5" SATA накопитель Samsung 870 EVO', '480 ГБ 2.5" SATA накопитель WD Green'], 2)),
     ([0, 0, 0, 0, 0, 0, 0, 0], ['Intel', 'Samsung', 'WD'], ([], 0)),
     ([1, 1, 1, 1, 0, 1, 1, 0], ['Intel', 'Samsung'], (['500 ГБ 2.5" SATA накопитель Samsung 870 EVO'], 1))
 )
+
+
 @pytest.mark.parametrize(
     'available, manufacturers, expected',
     params
 )
-
-
 def test_solve_with_params(create_list_of_models, available, manufacturers, expected):
     assert solve(create_list_of_models, available, manufacturers) == expected
+
+
+# Test reverse
+params = (
+    ('!dlroW olleH', 'hello world!'),
+    ('AvadaKedavraaaaA!', '!aaaaarvadekadava'),
+    ('', '')
+)
+
+
+@pytest.mark.parametrize(
+    'test_string, expected',
+    params
+)
+def test_reverse(test_string, expected):
+    assert reverse(test_string) == expected
+
+
+def test_reverse_int():
+    with pytest.raises(TypeError):
+        reverse(123)
+
+
+# Test check_email
+params = (
+    ('Helloworld@.ru', True),
+    ('мояпочта@нетология.ру', True),
+    ('python@email@net', False),
+    (' em@il.ru', False)
+)
+
+
+@pytest.mark.parametrize(
+    'email, expected',
+    params
+)
+def test_check_email(email, expected):
+    assert check_email(email) is expected
+
+
+def test_check_email_int():
+    with pytest.raises(TypeError):
+        check_email(123)
